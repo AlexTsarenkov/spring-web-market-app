133b4d1c2e1b71ee84766a2cad71aa7e5b3914f6
Alex Tsarenkov <alexey.tsarenkov@yandex.ru>
2026-02-12 12:47:05 +0300

    initial commit

---
+++ src/main/java/ru/yandex/praktikum/springwebmarketapp/SpringWebMarketAppApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.praktikum.springwebmarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMarketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMarketAppApplication.class, args);
+	}
+
+}
